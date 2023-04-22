52c691386f1abbab14cec970a5f9ac6f29914b0e
cchor <127997289+cchor@users.noreply.github.com>
2023-04-21 22:51:04 -0400

    made edits so it prompts the user to select all of the criteria for the password

--- script.js
+++ script.js
@@ -8,67 +8,24 @@ var specialCharacter = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "
 var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var pwReturn = "";
 
-function generatePassword(length) {
-
-  // var password ="";
-  // var passwordCharacter ="";
-  // var randomPw = "";
-  // var pwReturn = "";
-var minlength = 8;
-var maxlength = 128;
-
-  // how many characters
-  prompt("How many characters? 8 to 128")
-  // verify character count found on https://www.w3resource.com/javascript/form/string-length.php
-  function stringlength(inputtxt, minlength, maxlength)
-  { 
-  var field = inputtxt.value; 
-  var mnlen = minlength;
-  var mxlen = maxlength;
+function generatePassword() {
+  var userChoice = window.prompt("How many characters? 8 to 128");
+  // if (userChoice< 8 || userChoice> 128);
+  // alert("Please choose between 8 and 128 characters");
   
-  if(field.length< 8 || field.length> 128)
-  { 
-  alert("Please input the userid between " +mnlen+ " and " +mxlen+ " characters");
-  return false;
-  }
+  // } else if {
+    // if (userChoice> 8 || userChoice< 128);
+    confirm("Would you like to include upper case letters?"); 
+ 
+    confirm("Include lower case letters?");
+
+    confirm("Include numbers?");
+
+    confirm("Include symbols");
 
 }
-}
-  // uppercase?
-  var upperCaseconfirm = ("Include upper case?");
-  // lowercase?
-  var lowerCaseconfirm = ("Include lower case?");
-  // numbers?
-  var numbersConfirm = ("Include numbers?");
-  // special characters?
-  var specialCharactersconfirm = ("Include special characters?");
+ 
 
-//   if (upperCaseconfirm) {
-//     pwReturn += upperCase;
-// }
-
-//   if (lowerCaseconfirm) {
-//     pwReturn += lowerCase;
-// }
-
-// if (numbersConfirm) {
-//   pwReturn += number;
-// }
-
-// if (specialCharactersconfirm) {
-//   pwReturn += specialCharacter;
-// }
-
-// // Generates a random password based on the criteria that have been selected
-// for (var i = 0; i < passwordLength; i++) {
-//     // Continues to select a random character value from the string until it is the desired length
-//     randomPw += pwReturn.charAt(Math.floor(Math.random() * pwReturn.length));
-// }
-// password.value = pwReturn;
-
-// return "generated" 
-
-// }
 
 
 // Write password to the #password input - given info
@@ -81,5 +38,5 @@ function writePassword() {
 }
 
 
-// Add event listener to generate button
+// Add event listener to generate button - given info
 generateBtn.addEventListener("click", writePassword);
